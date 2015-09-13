e44e532443bfb71d0d9da25676ff068d26aeb9aa
pauldanielvidal <paul.daniel.r.vidal@gmail.com>
2015-09-14 00:51:19 +0800

    Committing updates for Status and Environment entities

---
+++ src/PVidal/Bundle/ChangelogDashboardBundle/Controller/EnvironmentController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace PVidal\Bundle\ChangelogDashboardBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use PVidal\Bundle\ChangelogDashboardBundle\Entity\Environment;
+
+
+class EnvironmentController extends Controller
+{
+
+    /**
+     * @Route("/env")
+     */
+    public function indexAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $this->getDoctrine()
+            ->getRepository('PVidalChangelogDashboardBundle:Environment')
+            ->findAll();
+
+        $columns = $em->getClassMetadata('PVidalChangelogDashboardBundle:Environment')->getFieldNames();
+
+        return $this->render('PVidalChangelogDashboardBundle:Default:list.html.twig', array(
+            'entities' => $entities,
+            'columns' => $columns,
+            'createLink' => "/env/new",
+            'type' => "Environment",
+        ));
+    }
+
+    /**
+     * @Route("/env/new")
+     */
+    public function newAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        // create a environment and give it some dummy data for this example
+        $environment = new Environment();
+
+        $form = $this->createFormBuilder($environment)
+            ->add('name', 'text')
+            ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->submit($request->request->get($form->getName()));
+
+            if ($form->isValid()) {
+                $environment = $form->getData();
+
+                $em->persist($environment);
+                $em->flush();
+
+                //return new Response("item created!".$environment->getId()." ".$environment->getDescription());
+                return $this->redirect('/env');
+            }
+        }
+
+        return $this->render('PVidalChangelogDashboardBundle:Default:new.html.twig', array(
+            'form' => $form->createView(),
+            'listLink' => "/env",
+        ));
+    }
+
+    /**
+     * @Route("/env/delete/{id}")
+     */
+    public function deleteAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $item = $this->getDoctrine()
+            ->getRepository('PVidalChangelogDashboardBundle:Environment')
+            ->find($request->get('id'));
+
+        $em->remove( $item );
+        $em->flush();
+
+        return $this->redirect('/env');
+    }
+}
---
+++ src/PVidal/Bundle/ChangelogDashboardBundle/Controller/StatusController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace PVidal\Bundle\ChangelogDashboardBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use PVidal\Bundle\ChangelogDashboardBundle\Entity\Status;
+
+
+class StatusController extends Controller
+{
+
+    /**
+     * @Route("/status")
+     */
+    public function indexAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $this->getDoctrine()
+            ->getRepository('PVidalChangelogDashboardBundle:Status')
+            ->findAll();
+
+        $columns = $em->getClassMetadata('PVidalChangelogDashboardBundle:Status')->getFieldNames();
+
+        return $this->render('PVidalChangelogDashboardBundle:Default:list.html.twig', array(
+            'entities' => $entities,
+            'columns' => $columns,
+            'createLink' => "/status/new",
+            'type' => "Status",
+        ));
+    }
+
+    /**
+     * @Route("/status/new")
+     */
+    public function newAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        // create a status and give it some dummy data for this example
+        $status = new Status();
+
+        $form = $this->createFormBuilder($status)
+            ->add('description', 'text')
+            ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->submit($request->request->get($form->getName()));
+
+            if ($form->isValid()) {
+                $status = $form->getData();
+
+                $em->persist($status);
+                $em->flush();
+
+                //return new Response("item created!".$status->getId()." ".$status->getDescription());
+                return $this->redirect('/status');
+            }
+        }
+
+        return $this->render('PVidalChangelogDashboardBundle:Default:new.html.twig', array(
+            'form' => $form->createView(),
+            'listLink' => "/status",
+        ));
+    }
+
+    /**
+     * @Route("/status/delete/{id}")
+     */
+    public function deleteAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $item = $this->getDoctrine()
+            ->getRepository('PVidalChangelogDashboardBundle:Status')
+            ->find($request->get('id'));
+
+        $em->remove( $item );
+        $em->flush();
+
+        return $this->redirect('/status');
+    }
+}
